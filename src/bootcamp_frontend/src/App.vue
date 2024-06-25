ba4309bd8419fbe88674bf965dcd00c4c84aa07b
majaszczepanska <139561097+majaszczepanska@users.noreply.github.com>
2024-06-25 16:17:54 +0200

    Update and rename App.js to App.vue

---
+++ src/bootcamp_frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <img src="/logo2.svg" alt="DFINITY logo" />
+    Hi
+    <br/>
+    <button @click="upper">upper</button>
+      {{ counter }}
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    upper() {
+      this.counter++
+    }
+  }
+}
+</script>
